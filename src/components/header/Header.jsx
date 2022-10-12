e56999367d092e605c5d72d8c4b477061881c53f
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2022-10-12 18:04:44 -0400

    Initial Set Up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import css from './header.css';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+const Header = ({ logo }) => {
+  const [isBurgerActive, setBurgerState] = useState(false);
+
+  function burgerClick() {}
+  return (
+    <header>
+      <div
+        className={
+          isBurgerActive ? 'header_container ' + 'active' : 'header_container'
+        }
+      >
+        <Link onClick={() => setBurgerState(false)} to='/'>
+          <img src={logo} alt='' />
+        </Link>
+        <div
+          onClick={() => setBurgerState(!isBurgerActive)}
+          className={isBurgerActive ? 'burger ' + 'active' : 'burger'}
+        ></div>
+        <ul className={isBurgerActive ? 'active' : ''}>
+          <HeaderLink onClick={() => setBurgerState(false)} to='/portfolio'>
+            Portfolio
+          </HeaderLink>
+          <HeaderLink onClick={() => setBurgerState(false)} to='/resume'>
+            Resume
+          </HeaderLink>
+          <HeaderLink onClick={() => setBurgerState(false)} to='/contact'>
+            Contact Me
+          </HeaderLink>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+function HeaderLink({ to, onClick, children, isBurgerActive, ...porps }) {
+  const resolvedPath = useResolvedPath(to);
+  const isCurrent = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isCurrent ? 'current' : ''}>
+      <Link onClick={onClick} to={to}>
+        {children}
+      </Link>
+    </li>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import css from './Home.css';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+function Home() {
+  return <div className='Home'>Home</div>;
+}
+
+export default Home;
